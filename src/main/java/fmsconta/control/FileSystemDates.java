7f570d6b3cc20c645a0263b0d643e8e8d314e44a
musef <musef2904@gmail.com>
2013-05-08 10:44:00 +0200

    Envio de ficheros relacionados con el DAO en fichero

---
+++ src/main/java/fmsconta/control/FileSystemDates.java
@@ -0,0 +1,9 @@
+package fmsconta.control;
+
+public interface FileSystemDates {
+	
+	
+	final String PathDataFiles="data/";
+	final String FileUsers="c_usuarios.txt";
+	final String FileCompanys="c_empresas.txt";
+}
