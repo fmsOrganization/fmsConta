60b6c2b033bd6d581461bd382a22f69c7bdfe9f9
musef <musef2904@gmail.com>
2013-05-13 16:24:57 +0200

    Nuevas modificaciones para ficheros

--- src/main/java/fmsconta/control/FileSystemDates.java
+++ src/main/java/fmsconta/control/FileSystemDates.java
@@ -1,9 +1,11 @@
 package fmsconta.control;
 
+import java.io.File;
+
 public interface FileSystemDates {
 	
 	
-	final String PathDataFiles="data/";
+	final String PathDataFiles="data"+File.separatorChar;
 	final String FileUsers="c_usuarios.txt";
 	final String FileCompanys="c_empresas.txt";
 }
