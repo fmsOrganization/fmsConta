3f69182aa89e04dd4277d2bc1e77c14e9d771d80
fmsmusef <musef_fms@terra.es>
2013-05-14 11:49:31 +0200

    Modificaciones y nuevos ficheros relacionados con los menús de informes. Incorporación de menú sumas y saldos versión Beta (retocar)

--- src/main/java/fmsconta/view/ContMnuMayor.java
+++ src/main/java/fmsconta/view/ContMnuMayor.java
@@ -1,5 +1,6 @@
 package fmsconta.view;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
@@ -10,6 +11,7 @@ import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -129,26 +131,157 @@ public class ContMnuMayor extends JFrame implements ActionListener, Settings {
 		Object source=e.getSource();
 		
 		if (source==listar) {
-			menuMayor.setVisible(false);
-			listaMayor=new ContListadoMayor("EC001","Ecovitalia",this.r1.getText(),this.r2.getText(),this.r3.getText(),this.r4.getText());
-			menuMayor.remove(space1);
-			menuMayor.remove(space2);
-			menuMayor.remove(north);
-			menuMayor.remove(space3);
-			menuMayor.remove(auxMenu);
-			menuMayor.remove(space4);
-			menuMayor.remove(south);
-			menuMayor.remove(south);
 			
-			menuMayor.add(listaMayor.retorna());
-			menuMayor.setPreferredSize(new Dimension(850,(listaMayor.sizeComponent*20)+125));
-			menuMayor.setAlignmentY(TOP_ALIGNMENT);
-			menuMayor.setVisible(true);
+			if (compruebaErrores()) {
+				
+				menuMayor.setVisible(false);
+				listaMayor=new ContListadoMayor("EC001","Ecovitalia",this.r1.getText(),this.r2.getText(),this.r3.getText(),this.r4.getText());
+				menuMayor.remove(space1);
+				menuMayor.remove(space2);
+				menuMayor.remove(north);
+				menuMayor.remove(space3);
+				menuMayor.remove(auxMenu);
+				menuMayor.remove(space4);
+				menuMayor.remove(south);
+				menuMayor.remove(south);
+				
+				menuMayor.add(listaMayor.retorna());
+				menuMayor.setPreferredSize(new Dimension(850,(listaMayor.sizeComponent*20)+125));
+				menuMayor.setAlignmentY(TOP_ALIGNMENT);
+				menuMayor.setVisible(true);
+			}
 		}
+		
 		
 		if (source==cancelar) {
 			menuMayor.setVisible(false);
 		}
 		
-	} 
+	} // fin del actionListener
+	
+	
+	
+	/* ****************************************************************************
+	 * Este metodo realiza la comprobacion de errores en introduccion formulario
+	 * 
+	 * No recibe parametros
+	 * Devuelve TRUE/FALSE segun este correcto o incorrecto el formulario
+	 ***************************************************************************** */
+	
+	private boolean compruebaErrores() {
+		
+		// Revision de errores
+		
+		r1.setBackground(Color.WHITE);
+		r2.setBackground(Color.WHITE);
+		r3.setBackground(Color.WHITE);
+		r4.setBackground(Color.WHITE);
+
+		
+		if (r1.getText().length()!=LongAuxiliares) {
+			JOptionPane.showMessageDialog(null, "Longitud de cuenta inadecuada");
+			r1.setBackground(Color.ORANGE);
+			return false;
+		} 
+		if (r2.getText().length()!=LongAuxiliares) {
+			JOptionPane.showMessageDialog(null, "Longitud de cuenta inadecuada");
+			r2.setBackground(Color.ORANGE);
+			return false;
+		}
+		if (r3.getText().length()!=10) {
+			JOptionPane.showMessageDialog(null, "Formato de fecha incorrecto: xx-xx-xxxx");
+			r3.setBackground(Color.ORANGE);
+			return false;
+		}
+		if (!compruebaFecha(r3.getText())) {
+			JOptionPane.showMessageDialog(null, "Formato de fecha incorrecto: xx-xx-xxxx");
+			r3.setBackground(Color.ORANGE);
+			return false;
+		}
+		if (r4.getText().length()!=10) {
+			JOptionPane.showMessageDialog(null, "Formato de fecha incorrecto: xx-xx-xxxx");
+			r4.setBackground(Color.ORANGE);
+			return false;
+		}
+		if (!compruebaFecha(r4.getText())) {
+			JOptionPane.showMessageDialog(null, "Formato de fecha incorrecto: xx-xx-xxxx");
+			r4.setBackground(Color.ORANGE);
+			return false;
+		}
+		
+		
+		return true;
+		
+	} // fin del metodo compruebaErrores
+	
+	
+	
+	/* ******************************************************************
+	 * Este metodo comprueba la fecha introducida y su correccion
+	 * 
+	 * Solo comprueba la concordancia dia y mes. No comprueba el año
+	 * Tampoco comprueba los años bisiestos
+	 * 
+	 * Recibe como argumento la fecha en formato String
+	 * Devuelve TRUE/FALSE según sea correcta o incorrecta la fecha
+	 **************************************************************** */
+	
+	private boolean compruebaFecha(String fecha) {
+		
+		
+		String dig1=fecha.substring(0,1);
+		String dig2=fecha.substring(1,2);
+		String dig3=fecha.substring(3,4);
+		String dig4=fecha.substring(4,5);
+		
+		if (!(dig1.equals("0") || dig1.equals("1") || dig1.equals("2") || dig1.equals("3"))) {
+			JOptionPane.showMessageDialog(null, "Formato fecha inicial: DD-MM 1");
+			return false;
+		}
+		if (!(dig2.equals("0") || dig2.equals("1") || dig2.equals("2") || dig2.equals("3") ||
+				dig2.equals("4") || dig2.equals("5") || dig2.equals("6") || dig2.equals("7") ||
+				dig2.equals("8") || dig2.equals("9"))) {
+			JOptionPane.showMessageDialog(null, "Formato fecha inicial: DD-MM 2");
+			return false;
+		}
+		if (!(dig3.equals("0") || dig3.equals("1"))) {
+			JOptionPane.showMessageDialog(null, "Formato fecha inicial: DD-MM 3");
+			return false;
+		}
+		if (!(dig4.equals("0") || dig4.equals("1") || dig4.equals("2") || dig4.equals("3") ||
+				dig4.equals("4") || dig4.equals("5") || dig4.equals("6") || dig4.equals("7") ||
+				dig4.equals("8") || dig4.equals("9"))) {
+			JOptionPane.showMessageDialog(null, "Formato fecha inicial: DD-MM 4");
+			return false;
+		}
+		
+		// comprobacion de correccion de fechas
+		int dia=(int)Integer.parseInt(fecha.substring(0,2));
+		int mes=(int)Integer.parseInt(fecha.substring(3,5));
+		
+		if (dia<1 || dia>31) {
+			JOptionPane.showMessageDialog(null, "El día "+dia+" no es correcto");
+			return false;
+		}
+		
+		if (mes<1 || mes>12) {
+			JOptionPane.showMessageDialog(null, "El mes "+mes+" no es correcto");
+			return false;
+		}
+		
+		if (mes==2 && dia>29) {
+			JOptionPane.showMessageDialog(null, "La fecha "+dia+"-"+mes+" no es correcta");
+			return false;
+		}
+		
+		if ((mes==4 || mes==6 || mes==9 || mes==11 ) && dia>30) {
+			JOptionPane.showMessageDialog(null, "La fecha "+dia+"-"+mes+" no es correcta");
+			return false;
+		}
+		
+		// llegado aqui, la fecha es correcta
+		return true;
+		
+	} // fin del metodo compruebaFecha 
+	
 }   // ***************** fin de la class ContMnuMayor
--- src/main/java/fmsconta/view/ContMnuSumasSaldos.java
+++ src/main/java/fmsconta/view/ContMnuSumasSaldos.java
@@ -1,5 +1,6 @@
 package fmsconta.view;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
@@ -11,6 +12,7 @@ import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -35,13 +37,14 @@ public class ContMnuSumasSaldos extends JFrame implements ActionListener, Settin
 	private JButton listar;
 	private JButton cancelar;
 	
-	private ContListadoMayor listaMayor;
+	private ContListadoSyS listaSumas;
 	
 	
 	
 	/* ****************************************************************************
 	 * 
-	 * Este metodo constructor implementa el menu de impresion del Mayor contable
+	 * Este metodo constructor implementa el menu de impresion del Balance
+	 * de Sumas y Saldos
 	 * 
 	 * ************************************************************************** */
 	
@@ -150,26 +153,169 @@ public class ContMnuSumasSaldos extends JFrame implements ActionListener, Settin
 		Object source=e.getSource();
 		
 		if (source==listar) {
-			menuSumas.setVisible(false);
-			listaMayor=new ContListadoMayor("EC001","Ecovitalia",this.r1.getText(),this.r2.getText(),this.r3.getText(),this.r4.getText());
-			menuSumas.remove(space1);
-			menuSumas.remove(space2);
-			menuSumas.remove(north);
-			menuSumas.remove(space3);
-			menuSumas.remove(auxMenu);
-			menuSumas.remove(space4);
-			menuSumas.remove(south);
-			menuSumas.remove(south);
-			
-			menuSumas.add(listaMayor.retorna());
-			menuSumas.setPreferredSize(new Dimension(850,(listaMayor.sizeComponent*20)+125));
-			menuSumas.setAlignmentY(TOP_ALIGNMENT);
-			menuSumas.setVisible(true);
+					
+			if (compruebaErrores()) {
+				// no habiendo errores
+				menuSumas.setVisible(false);
+				listaSumas=new ContListadoSyS("EC001","Ecovitalia",this.r1.getText(),this.r2.getText(),this.r3.getText(),this.r4.getText());
+				menuSumas.remove(space1);
+				menuSumas.remove(space2);
+				menuSumas.remove(north);
+				menuSumas.remove(space3);
+				menuSumas.remove(auxMenu);
+				menuSumas.remove(space4);
+				menuSumas.remove(south);
+				menuSumas.remove(south);
+
+				menuSumas.add(listaSumas.retorna());
+				menuSumas.setPreferredSize(new Dimension(850,(listaSumas.sizeComponent*20)+125));
+				menuSumas.setAlignmentY(TOP_ALIGNMENT);
+				menuSumas.setVisible(true);
+				
+			}
+
 		}
 		
 		if (source==cancelar) {
 			menuSumas.setVisible(false);
 		}
 		
-	} 
+	} // fin del ActionLister
+	
+	
+	
+	
+	/* ****************************************************************************
+	 * Este metodo realiza la comprobacion de errores en introduccion formulario
+	 * 
+	 * No recibe parametros
+	 * Devuelve TRUE/FALSE segun este correcto o incorrecto el formulario
+	 ***************************************************************************** */
+	
+	private boolean compruebaErrores() {
+		
+		// Revision de errores
+		
+		r1.setBackground(Color.WHITE);
+		r2.setBackground(Color.WHITE);
+		r3.setBackground(Color.WHITE);
+		r4.setBackground(Color.WHITE);
+		r5.setBackground(Color.WHITE);
+		
+		if (r1.getText().length()!=LongAuxiliares) {
+			JOptionPane.showMessageDialog(null, "Longitud de cuenta inadecuada");
+			r1.setBackground(Color.ORANGE);
+			return false;
+		} 
+		if (r2.getText().length()!=LongAuxiliares) {
+			JOptionPane.showMessageDialog(null, "Longitud de cuenta inadecuada");
+			r2.setBackground(Color.ORANGE);
+			return false;
+		}
+		if (r3.getText().length()!=10) {
+			JOptionPane.showMessageDialog(null, "Formato de fecha incorrecto: xx-xx-xxxx");
+			r3.setBackground(Color.ORANGE);
+			return false;
+		}
+		if (!compruebaFecha(r3.getText())) {
+			JOptionPane.showMessageDialog(null, "Formato de fecha incorrecto: xx-xx-xxxx");
+			r3.setBackground(Color.ORANGE);
+			return false;
+		}
+		if (r4.getText().length()!=10) {
+			JOptionPane.showMessageDialog(null, "Formato de fecha incorrecto: xx-xx-xxxx");
+			r4.setBackground(Color.ORANGE);
+			return false;
+		}
+		if (!compruebaFecha(r4.getText())) {
+			JOptionPane.showMessageDialog(null, "Formato de fecha incorrecto: xx-xx-xxxx");
+			r4.setBackground(Color.ORANGE);
+			return false;
+		}
+		if (r5.getText().length()!=1) {
+			JOptionPane.showMessageDialog(null, "Error en nivel de cuentas (3, 4 o "+LongAuxiliares+")");
+			r5.setBackground(Color.ORANGE);
+			return false;
+		}
+		if (!(r5.getText().equals("3") || r5.getText().equals("4") || r5.getText().equals(String.valueOf(LongAuxiliares)))) {
+			JOptionPane.showMessageDialog(null, "Error en nivel de cuentas (3, 4 o "+LongAuxiliares+")");
+			r5.setBackground(Color.ORANGE);
+			return false;
+		}
+		
+		
+		return true;
+		
+	} // fin del metodo compruebaErrores
+	
+	
+	
+	/* ******************************************************************
+	 * Este metodo comprueba la fecha introducida y su correccion
+	 * 
+	 * Solo comprueba la concordancia dia y mes. No comprueba el año
+	 * Tampoco comprueba los años bisiestos
+	 * 
+	 * Recibe como argumento la fecha en formato String
+	 * Devuelve TRUE/FALSE según sea correcta o incorrecta la fecha
+	 **************************************************************** */
+	
+	private boolean compruebaFecha(String fecha) {
+		
+		
+		String dig1=fecha.substring(0,1);
+		String dig2=fecha.substring(1,2);
+		String dig3=fecha.substring(3,4);
+		String dig4=fecha.substring(4,5);
+		
+		if (!(dig1.equals("0") || dig1.equals("1") || dig1.equals("2") || dig1.equals("3"))) {
+			JOptionPane.showMessageDialog(null, "Formato fecha inicial: DD-MM 1");
+			return false;
+		}
+		if (!(dig2.equals("0") || dig2.equals("1") || dig2.equals("2") || dig2.equals("3") ||
+				dig2.equals("4") || dig2.equals("5") || dig2.equals("6") || dig2.equals("7") ||
+				dig2.equals("8") || dig2.equals("9"))) {
+			JOptionPane.showMessageDialog(null, "Formato fecha inicial: DD-MM 2");
+			return false;
+		}
+		if (!(dig3.equals("0") || dig3.equals("1"))) {
+			JOptionPane.showMessageDialog(null, "Formato fecha inicial: DD-MM 3");
+			return false;
+		}
+		if (!(dig4.equals("0") || dig4.equals("1") || dig4.equals("2") || dig4.equals("3") ||
+				dig4.equals("4") || dig4.equals("5") || dig4.equals("6") || dig4.equals("7") ||
+				dig4.equals("8") || dig4.equals("9"))) {
+			JOptionPane.showMessageDialog(null, "Formato fecha inicial: DD-MM 4");
+			return false;
+		}
+		
+		// comprobacion de correccion de fechas
+		int dia=(int)Integer.parseInt(fecha.substring(0,2));
+		int mes=(int)Integer.parseInt(fecha.substring(3,5));
+		
+		if (dia<1 || dia>31) {
+			JOptionPane.showMessageDialog(null, "El día "+dia+" no es correcto");
+			return false;
+		}
+		
+		if (mes<1 || mes>12) {
+			JOptionPane.showMessageDialog(null, "El mes "+mes+" no es correcto");
+			return false;
+		}
+		
+		if (mes==2 && dia>29) {
+			JOptionPane.showMessageDialog(null, "La fecha "+dia+"-"+mes+" no es correcta");
+			return false;
+		}
+		
+		if ((mes==4 || mes==6 || mes==9 || mes==11 ) && dia>30) {
+			JOptionPane.showMessageDialog(null, "La fecha "+dia+"-"+mes+" no es correcta");
+			return false;
+		}
+		
+		// llegado aqui, la fecha es correcta
+		return true;
+		
+	} // fin del metodo compruebaFecha 
+	
 }   // ***************** fin de la class ContMnuSumasSaldos
